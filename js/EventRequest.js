477af4fecaa217d54526db1884d8c2e0dbcf2970
darkucfknight <jordan.layton95@gmail.com>
2017-03-25 15:53:34 -0400

    add event request js

---
+++ js/EventRequest.js
@@ -0,0 +1,55 @@
+$(function() {
+	$('#errorAlert').hide();
+	$('#successAlert').hide();
+	$('#submitBtn').on('click', submitRequest);
+
+	function submitRequest()
+	{
+		if(!validateForm())
+		{
+			return;
+		}
+
+		//TODO Form suibmission
+
+		//on success
+		$('#successAlert').show();
+	};
+
+	function validateForm()
+	{
+		$('#errorAlert').hide();
+		$('#successAlert').hide();
+		var errors = 0;
+		var time_regex = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/;
+		var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
+		var email_regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		var phone_regex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
+
+		if($('#title').val() == "")
+			errors++;
+
+		if($('#description').val() == "")
+			errors++;
+
+		if(!date_regex.test($('#date').val()))
+			errors++;
+
+		if(!time_regex.test($('#time').val()))
+			errors++;
+
+		if(!phone_regex.test($('#contact_phone').val()))
+			errors++;
+
+		if(!email_regex.test($('#contact_email').val()))
+			errors++;
+
+	    if(errors>0)
+	    {
+	    	console.log(errors);
+	    	$('#errorAlert').show();
+	    	return 0;
+	    }
+	    return 1;
+	}
+});
