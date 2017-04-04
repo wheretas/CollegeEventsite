a460523703ed6e10bfc6d74c143955af0cfbd812
wheretas <wheretas@knights.ucf.edu>
2017-04-04 19:06:58 -0400

    added login and create page. Also integrated the backend

--- config.php
+++ config.php
@@ -1,7 +1,31 @@
 <?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'root123');
-   define('DB_DATABASE', 'eventsite');
-   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+$dbhost = "localhost";
+$dbuser = "web";
+$dbpass = "localpassword";
+$dbname = "eventsite";
+$db = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname) or die("cannot connect"); 
+
+ 
+mysqli_select_db($db, $dbname) or die("cannot select DB");
+
+$email =      $_POST ['email'];
+$first_name =       $_POST ['first_name'];
+$last_name=  $_POST['last_name'];
+$password =           $_POST ['password'];
+
+
+
+$query = 
+"INSERT INTO users
+(email,first_name,last_name,password)
+
+values
+
+('$email','$first_name','$last_name','$password')";
+
+$result = mysqli_query($db, $query);
+Echo "Database Saved"; 
+mysqli_close($db);
+
 ?>
