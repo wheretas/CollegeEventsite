5c7f814beb2ae4f9b934f926e56e2d9829d9fba9
darkucfknight <jordan.layton95@gmail.com>
2017-04-17 16:21:10 -0400

    Comment section comment types
    adds comment section comment types on event.html and a commented out fb
    button that will be fixed soon

--- js/Event.js
+++ js/Event.js
@@ -3,66 +3,34 @@
 $(function() {
 	$('#commentErrorAlert').hide();
 	$('#commentSuccessAlert').hide();
+	$('#submit').hide();
+	$('#cancel').hide();
+	$('#rating').prop("disabled", true);
+	$('#comment').prop("disabled", true);
 
-	function submitRequest()
-	{
-		if(!validateForm())
-		{
-			return;
-		}
+	$('#edit').on('click', function() {
+		$('#rating').prop("disabled", false);
+		$('#comment').prop("disabled", false);
+		$('#edit').hide();
+		$('#delete').hide();
+		$('#submit').show();
+		$('#cancel').show();
+	});
 
-		//TODO Form suibmission
+	$('#delete').on('click', function() {
+		//TODO delete comment from dba and reload page
+	});
 
-		//on success
-		$('#successAlert').show();
-	};
+	$('#submit').on('click', function() {
+		//Change comment values in db and reload page
+	});
 
-	function resetForm()
-	{
-		$('#genInfo').find("textarea, input, select").val('').end();
-	}
-
-	function validateForm()
-	{
-		$('#errorAlert').hide();
-		$('#successAlert').hide();
-		var errors = 0;
-		var time_regex = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/;
-		var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/;
-		var email_regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-		var phone_regex = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/;
-
-		if($('#title').val() == "")
-			errors++;
-
-		if($('#description').val() == "")
-			errors++;
-
-		if(!date_regex.test($('#date').val()))
-			errors++;
-
-		if(!time_regex.test($('#time').val()))
-			errors++;
-
-		if(!phone_regex.test($('#contact_phone').val()))
-			errors++;
-
-		if(!email_regex.test($('#contact_email').val()))
-			errors++;
-
-	    if(errors>0)
-	    {
-	    	console.log(errors);
-	    	$('#errorAlert').show();
-	    	return 0;
-	    }
-	    return 1;
-	}
-
-	function resetLoc()
-	{
-		document.getElementById('latitude').value=document.getElementById('us3-lat').value;
-		document.getElementById('longitude').value=document.getElementById('us3-lon').value;
-
-	}
+	$('#cancel').on('click', function() {
+		$('#rating').prop("disabled", true);
+		$('#comment').prop("disabled", true);
+		$('#submit').hide();
+		$('#cancel').hide();
+		$('#edit').show();
+		$('#delete').show();
+	});
 });
