8032bb02d3b1cdc8d342416e6eec51550f21a137
darkucfknight <jordan.layton95@gmail.com>
2017-04-17 17:53:19 -0400

    Add front-end side of adding user to an RSO
    still need to add code to actually add user to the rso in the db

--- js/RSOSearch.js
+++ js/RSOSearch.js
@@ -1,3 +1,25 @@
 $(function() {
     $('#RSOTable').DataTable();
+
+    $('.clickable-row').click(function() {
+    	var table = document.getElementById('RSOTable');
+    	var cell = table.rows.item($(this).index()+1).cells;
+
+    	//the name of the RSO 
+    	var RSOName = cell.item(0).innerHTML;
+
+    	$.confirm({
+		    title: 'Do You Wish To Join "'+RSOName+'"?',
+		    content: 'Click CONFIRM to join this RSO and see its events in your event feed.',
+		    buttons: {
+		        confirm: function () {
+		        	//TODO add user to this RSO
+		            $.alert('You have joined '+RSOName);
+		        },
+		        cancel: function () {
+		            $.alert('You did not join '+RSOName);
+		        }		       
+		    }
+		});
+    });
 });
