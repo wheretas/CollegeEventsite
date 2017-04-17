7c6ec56b716d58f1d96b8f943ad6d9dcc9671470
darkucfknight <jordan.layton95@gmail.com>
2017-04-16 23:34:23 -0400

    clickable rows in index and myevents
    clicking on an event row now takes you to an event page with a key
    contained in the url dispaying the event id. this key variable will be
    used to populate the event data with php

--- js/myEvents.js
+++ js/myEvents.js
@@ -1,3 +1,11 @@
 $(function() {
     $('#myEventsTable').DataTable();
+
+    $('.clickable-row').click(function() {
+    	var table = document.getElementById('myEventsTable');
+    	var cell = table.rows.item($(this).index()+1).cells;
+    	var cellval = cell.item(0).innerHTML;
+
+        window.location = $(this).data("href")+cellval;
+    });
 });
