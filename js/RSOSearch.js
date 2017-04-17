ebdffa8d3c642e798cbd6c6df5f2cf4224976109
darkucfknight <jordan.layton95@gmail.com>
2017-04-17 19:54:17 -0400

    table now displays correct university, not email
    converted my js schoolFromEmail func to php.easy.

--- js/RSOSearch.js
+++ js/RSOSearch.js
@@ -22,4 +22,20 @@ $(function() {
 		    }
 		});
     });
+
+    function schoolFromEmail(email) 
+	{
+		var address = email.toString().substring(email.indexOf('@')+1, email.length);
+		if(address.includes("ucf"))
+			return 'UCF';
+		if(address.includes("uf"))
+			return 'UF';
+		if(address.includes("fsu"))
+			return 'FSU';
+		if(address.includes("gt"))
+			return 'GT';
+		else
+			return 'N/A';
+
+	}
 });
