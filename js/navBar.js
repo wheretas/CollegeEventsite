6ccefa1a05384bb786c7bead67fe96a939fd87c9
darkucfknight <jordan.layton95@gmail.com>
2017-04-16 21:23:04 -0400

    nav bar shows and hides links for different u types

--- js/navBar.js
+++ js/navBar.js
@@ -1,28 +1,37 @@
-/*$(function() {
+$(function() {
 	$('#createEventNav').hide();
     $('#createUniNav').hide();
     var userRole=getUserRole();
 
-    if(userRole>=1)
+    //console.log('userole: '+userRole);
+
+    /*if(userRole>=1)
     {
     	$('#createEventNav').show();
     }
     if(userRole==2)
     {
         $('#createUniNav').show();
-    }
+    }*/
 
     function getUserRole() {
+        var realUserRole;
     	var tempUserRole=$.post("getUserRole.php", function() {
-    		alert("ajax called");
     	})
-    	.done(function() {
-    		alert("ajax success");
+    	.done(function(resp) {
+            realUserRole = JSON.stringify(resp).substring(5,6);
+            if(realUserRole>=2)
+            {
+                $('#createEventNav').show();
+            }
+            if(realUserRole==3)
+            {
+                $('#createUniNav').show();
+            }
     	})
     	.fail(function() {
     		alert("ajax failure");
     	})
-    	return 1;
+    	return realUserRole;
     }
 });
-*/
