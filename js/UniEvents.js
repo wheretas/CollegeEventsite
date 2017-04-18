ea8eba12f8a9338447bf29f62b8306f59f783030
darkucfknight <jordan.layton95@gmail.com>
2017-04-17 21:14:30 -0400

    adds clickable rows to UniEvents
    clicking onevents in univeventes table now takes you to event page

--- js/UniEvents.js
+++ js/UniEvents.js
@@ -1,7 +1,13 @@
 $(function() {
     $('#EventTable').DataTable();
 
+    $('.clickable-row').click(function() {
+    	var table = document.getElementById('EventTable');
+    	var cell = table.rows.item($(this).index()+1).cells;
+    	var cellval = cell.item(0).innerHTML;
 
+        window.location = $(this).data("href")+cellval;
+    });
 
 
     function schoolFromEmail(email) 
